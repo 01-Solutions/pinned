59ee2a667275b9e9114afaae7b4c75a20c641225
ahmed <ahmedsh717@gmail.com>
2020-07-16 22:41:42 +0300

    server1

--- server.js
+++ server.js
@@ -27,34 +27,78 @@ const url = `https://newsapi.org/v2/everything?q=topNews&apiKey=${key}`;
 // if the user is a 'gust' then we will send hem to this route
 server.get('/', test);
 
-//if the user is a Signed up user then we will send hem to this rout
-server.get('/home',getHomeData);
+// if the user is a Signed up user then we will send hem to this rout
+server.get('/home', getHomeData);
 
 
-function getHomeData(req,res) {
+let myArrticle = [];
+var result;
+function getHomeData(req, res) {
     let sql = `select interests.interest_desc from interests,users_interests where interests.interest_id = users_interests.interest_id and users_interests.user_id = 1;`;
-    client.query(sql).then(result =>{
-        res.send(arrToObj(result.rows,interest_desc))
+    client.query(sql)
+    .then(sqlResult => {
+        result = arrToObj(sqlResult.rows, 'interest_desc');
+    })
+    .then(()=>{
+        // console.log(result);
+        result.forEach(item => {
+            let myUrl = `https://newsapi.org/v2/everything?q=${item}&apiKey=${key}`
+            agent.get(myUrl)
+            .then(data => {
+                let APIResult = JSON.parse(data.text).articles
+                for (let index = 0; index < 3; index++) {
+                    
+                    myArrticle.push(new Article(APIResult[index]));
+                    
+                }
+                // console.log(myArrticle);
+                console.log('I am first');
+            })    
+            
+        })
+    })
+    .end(()=>{
+        res.status(200).send(myArrticle);
     });
+    console.log('I am secound');
 }
+// function getHomeData(req, res) {
+//     let sql = `select interests.interest_desc from interests,users_interests where interests.interest_id = users_interests.interest_id and users_interests.user_id = 1;`;
+//     client.query(sql).then(sqlResult => { // res.send(arrToObj(sqlResult.rows,'interest_desc'))
+//         result = arrToObj(sqlResult.rows, 'interest_desc');
+//         result.forEach(item => {
+//             let myUrl = `https://newsapi.org/v2/everything?q=${item}&apiKey=${key}`
+//             agent.get(myUrl).then(data => {
+//                 let APIResult = JSON.parse(data.text).articles
+//                 for (let index = 0; index < 3; index++) {
+
+//                         myArrticle.push(new Article(APIResult[index]));
+                    
+//                 }
+//             });
+//         })
+//     }).then(()=>{
+//         res.status(200).send(myArrticle);
+
+//     });
+// }
 // this is a fuction to transfare array of objects to array
-function arrToObj(arr,myProperty) {
-    let result = arr.map(item =>{
-        console.log(item.myProperty);
-        return item.myProperty;
+function arrToObj(arr, myProperty) {
+    let result = arr.map(item => {
+        return item[myProperty];
     });
     return result;
 }
 
 function test(req, res) {
-agent.get(url).then(result=>{
-    let APIResult = JSON.parse(result.text).articles
-    let myArticls = APIResult.map(item=>{
-        return new Article(item);
+    agent.get(url).then(result => {
+        let APIResult = JSON.parse(result.text).articles
+        let myArticls = APIResult.map(item => {
+            return new Article(item);
 
+        });
+        res.status(200).send(myArticls);
     });
-    res.status(200).send(myArticls);
-});
 };
 
 function Article(articleData) {
@@ -62,12 +106,12 @@ function Article(articleData) {
     this.author = articleData.author;
     this.img = articleData.urlToImage;
     this.url = articleData.url;
-    this.source = (articleData.source.name)? articleData.source.name : 'Ahmad Shela';
+    this.source = (articleData.source.name) ? articleData.source.name : 'Ahmad Shela';
     this.content = articleData.content;
 }
 
 
-client.connect().then(()=>{
+client.connect().then(() => {
     server.listen(PORT, () => {
         console.log('I am linstining on port : ', PORT);
     });
