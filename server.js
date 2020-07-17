f4d57d25e0cbc00a2c7156cd4d2da32bee9eaff8
ahmed <ahmedsh717@gmail.com>
2020-07-17 15:59:44 +0300

    add home routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const https = require('https');
 const cors = require('cors');
 const agent = require('superagent');
 const pgSQL = require('pg');
@@ -18,50 +19,65 @@ const PORT = process.env.PORT || 3030;
 /****************************************** */
 const key = process.env.NEWSKEY;
 const url = `https://newsapi.org/v2/everything?q=topNews&apiKey=${key}`;
-// server.get('/', getPage);
+server.get('/', getPage);
 
-// function getPage(req, res) {
-//     res.render('pages/index');
-// }
+function getPage(req, res) {
+    res.render('pages/index');
+}
 
 // if the user is a 'gust' then we will send hem to this route
-server.get('/', test);
+server.get('/test', test);
 
 // if the user is a Signed up user then we will send hem to this rout
 server.get('/home', getHomeData);
 
 
-let myArrticle = [];
-var result;
-function getHomeData(req, res) {
+function getHomeData(req,res) {
+    var sqlResult = [];
+    var finalResult = [];
     let sql = `select interests.interest_desc from interests,users_interests where interests.interest_id = users_interests.interest_id and users_interests.user_id = 1;`;
     client.query(sql)
-    .then(sqlResult => {
-        result = arrToObj(sqlResult.rows, 'interest_desc');
-    })
-    .then(()=>{
-        // console.log(result);
-        result.forEach(item => {
-            let myUrl = `https://newsapi.org/v2/everything?q=${item}&apiKey=${key}`
+    .then(sqlData => { // get the SQL result
+        sqlResult = arrToObj(sqlData.rows, 'interest_desc')
+        console.log('********************************First');
+    }).then(() => { // get the API result based on the SQL result
+        sqlResult.forEach(item => {
+            console.log(item);
+            let myUrl = `https://newsapi.org/v2/everything?q=${item}&apiKey=${key}`;
             agent.get(myUrl)
-            .then(data => {
-                let APIResult = JSON.parse(data.text).articles
-                for (let index = 0; index < 3; index++) {
-                    
-                    myArrticle.push(new Article(APIResult[index]));
-                    
+            .then(apiResult => { // filtt the array of result
+                if (JSON.parse(apiResult.text).articles.length >= 3) {
+                    for (let i = 0; i < 3; i++) {
+                        finalResult.push(new Article(JSON.parse(apiResult.text).articles[i]))
+                    }
                 }
-                // console.log(myArrticle);
-                console.log('I am first');
-            })    
-            
+                console.log('********************************secound');
+            })
         })
+        
+    }).then(() => {
+        // resoned the array of result
+        // setTimeout(console.log('third'),3000);
+        setTimeout(function () {
+            console.log(finalResult.length);
+            res.send(finalResult)
+            console.log('********************************third');
+        }, (700));
     })
-    .end(()=>{
-        res.status(200).send(myArrticle);
-    });
-    console.log('I am secound');
 }
+function test(arr) {
+    let arrResult = [];
+    arr.forEach(item => {
+
+        agent.get(myUrl).then(result => {
+            for (let i = 0; i < 3; i++) {
+                arrResult.push(result[i])
+            }
+        });
+    });
+    return arrResult
+}
+
 // function getHomeData(req, res) {
 //     let sql = `select interests.interest_desc from interests,users_interests where interests.interest_id = users_interests.interest_id and users_interests.user_id = 1;`;
 //     client.query(sql).then(sqlResult => { // res.send(arrToObj(sqlResult.rows,'interest_desc'))
@@ -73,7 +89,7 @@ function getHomeData(req, res) {
 //                 for (let index = 0; index < 3; index++) {
 
 //                         myArrticle.push(new Article(APIResult[index]));
-                    
+
 //                 }
 //             });
 //         })
