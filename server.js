cba5be61c75e93fabee191c89754bf234dfa7ea5
laith <mr.layth_2011@hotmail.com>
2020-07-19 15:34:40 +0300

    finishSearchAndBut

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const https = require('https');
 const cors = require('cors');
 const agent = require('superagent');
 const pgSQL = require('pg');
-var cheerio = require('cheerio'),
-    $ = cheerio.load('pages/index.ejs');
+// var cheerio = require('cheerio'),
+// $ = cheerio.load('pages/index.ejs');
 const server = express();
 const client = new pgSQL.Client(process.env.DATABASE_URL)
 server.use(cors());
@@ -44,22 +44,22 @@ server.get('/sign/signin-sigup', (req, res) => {
     res.render('./pages/signin-sigup')
 });
 
-function getHomeData(req,res) {
+function getHomeData(req, res) {
     var sqlResult = [];
     var finalResult = [];
     let sql = `select interests.interest_desc from interests,users_interests where interests.interest_id = users_interests.interest_id and users_interests.user_id = 1;`;
     client.query(sql)
-    .then(sqlData => { // get the SQL result
-        sqlResult = arrToObj(sqlData.rows, 'interest_desc')
-        sqlResult = sqlResult.join(' OR ')
-        let myURL = `https://newsapi.org/v2/everything?q=(${sqlResult})&apiKey=${key}`;
-        agent.get(myURL).then(apiResult =>{
-           let result= JSON.parse(apiResult.text).articles.map(item=>{
-                 return new Article(item);
-            })
-            res.send(result)
-        });
-    })
+        .then(sqlData => { // get the SQL result
+            sqlResult = arrToObj(sqlData.rows, 'interest_desc')
+            sqlResult = sqlResult.join(' OR ')
+            let myURL = `https://newsapi.org/v2/everything?q=(${sqlResult})&apiKey=${key}`;
+            agent.get(myURL).then(apiResult => {
+                let result = JSON.parse(apiResult.text).articles.map(item => {
+                    return new Article(item);
+                })
+                res.send(result)
+            });
+        })
 }
 
 // this is a fuction to transfare array of objects to array
@@ -78,39 +78,39 @@ function test(req, res) {
             return new Article(item);
         });
         // console.log(myArticls);
-        res.render('pages/articls', {articlsKey: myArticls});
+        res.render('pages/articls', { articlsKey: myArticls });
     });
 };
 
 /* get data from sign in form */
-function signinFun(req, res){
+function signinFun(req, res) {
     var email = req.body.Email;
     var password = req.body.Password;
     console.log(email);
     console.log(password);
     let sql = `select * from users where user_email = '${email}';`;
     console.log(sql);
-    client.query(sql).then(dbResult =>{
-        // console.log(dbResult);
-        if(dbResult.rows.length > 0){
-            if(dbResult.rows[0].user_pass == password){
-                console.log('goog job');
-                res.redirect('/home')
+    client.query(sql).then(dbResult => {
+            // console.log(dbResult);
+            if (dbResult.rows.length > 0) {
+                if (dbResult.rows[0].user_pass == password) {
+                    console.log('goog job');
+                    res.redirect('/home')
 
-            }else{
-                res.render('pages/signin-sigup', {result: 'PassFalse'});
+                } else {
+                    res.render('pages/signin-sigup', { result: 'PassFalse' });
+                }
+            } else {
+                console.log(test);
+                res.render('pages/signin-sigup', { result: 'SignUp' });
             }
-        }else{
-            console.log(test);
-            res.render('pages/signin-sigup', {result: 'SignUp'});
-        }
-    })
-    // res.render('pages/signin-sigup', {});
+        })
+        // res.render('pages/signin-sigup', {});
 }
 
 
 /* get data from sign up form */
-function signupFun(req, res){
+function signupFun(req, res) {
     var userName = req.body.UserName;
     var email = req.body.Email;
     var password = req.body.Password;
@@ -131,7 +131,7 @@ function Article(articleData) {
     this.url = articleData.url;
     this.source = (articleData.source.name) ? articleData.source.name : 'Ahmad Shela';
     this.description = articleData.description;
-    this.date=new Date(articleData.publishedAt).toDateString();
+    this.date = new Date(articleData.publishedAt).toDateString();
 }
 client.connect().then(() => {
     server.listen(PORT, () => {
