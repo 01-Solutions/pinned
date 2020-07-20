ca0430129c705bad6c4813c4387806cbb074dcd7
ahmad-swedani <swedani390@gmail.com>
2020-07-20 13:17:27 +0300

    solve promises

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const PORT = process.env.PORT || 3030;
 /****************************************** */
 const key = process.env.NEWSKEY;
 const url = `https://newsapi.org/v2/everything?q=latest&apiKey=${key}`;
-var user_id ;
+var user_id;
+var user_email;
 function getPage(req, res) {
     // res.render('pages/index');
 }
@@ -40,6 +41,9 @@ server.post('/signup', signupFun);
 /*this route for get search reslut */
 server.post('/search', getSearchResult);
 
+server.post('/getUserEmail', (req, res) => {
+    user_email = req.body.email;
+});
 /* this route for sinein data */
 server.post('/signupdata', dataTOsignin);
 
@@ -48,29 +52,29 @@ server.get('/sign/signin-sigup', (req, res) => {
     res.render('./pages/signin-sigup')
 });
 
-function dataTOsignin(req, res){
+function dataTOsignin(req, res) {
     var datasignin = req.body.msg;
-    res.render('pages/signin-sigup',{singinMsg: datasignin})
+    res.render('pages/signin-sigup', { singinMsg: datasignin })
 }
 
 function getHomeData(req, res) {
     var sqlResult = [];
-    let sql = `select interests.interest_desc from interests,users_interests where interests.interest_id = users_interests.interest_id and users_interests.user_id = ${user_id};`;
+    let sql = `select interests.interest_desc from interests,users_interests where interests.interest= users_interests.interest_id and users_interests.user_id = ${user_id};`;
     client.query(sql)
-    .then(sqlData => { // get the SQL result
-        if(sqlData.rows.length < 1){
-            res.redirect('/')
-        }
-        sqlResult = arrToObj(sqlData.rows, 'interest_desc')
-        sqlResult = sqlResult.join(' OR ')
-        let myURL = `https://newsapi.org/v2/everything?q=(${sqlResult})&apiKey=${key}`;
-        agent.get(myURL).then(apiResult =>{
-           let result= JSON.parse(apiResult.text).articles.map(item=>{
-                 return new Article(item);
+        .then(sqlData => { // get the SQL result
+            if (sqlData.rows.length < 1) {
+                res.redirect('/')
+            }
+            sqlResult = arrToObj(sqlData.rows, 'interest_desc')
+            sqlResult = sqlResult.join(' OR ')
+            let myURL = `https://newsapi.org/v2/everything?q=(${sqlResult})&apiKey=${key}`;
+            agent.get(myURL).then(apiResult => {
+                let result = JSON.parse(apiResult.text).articles.map(item => {
+                    return new Article(item);
                 })
-            res.render('pages/index', {allArticles: result});
-        });
-    })
+                res.render('pages/index', { allArticles: result });
+            });
+        })
 }
 // this is a fuction to transfare array of objects to array
 function arrToObj(arr, myProperty) {
@@ -90,13 +94,13 @@ function test(req, res) {
         });
 
 
-        res.render('pages/index', {allArticles: myArticls});
+        res.render('pages/index', { allArticles: myArticls });
     });
 };
-function dataTOsignin(req, res){
+function dataTOsignin(req, res) {
     var datasignin = req.body.msg;
     console.log(datasignin);
-    res.render('pages/signin-sigup',{singinMsg: datasignin})
+    res.render('pages/signin-sigup', { singinMsg: datasignin })
 }
 /* get data from sign in form */
 function signinFun(req, res) {
@@ -104,96 +108,127 @@ function signinFun(req, res) {
     var password = req.body.Password;
 
     let sql = `select * from users where user_email = '${email}';`;
-    client.query(sql).then(dbResult =>{
-        if(dbResult.rows.length > 0){
-            if(dbResult.rows[0].user_pass == password){
+    client.query(sql).then(dbResult => {
+        if (dbResult.rows.length > 0) {
+            if (dbResult.rows[0].user_pass == password) {
                 user_id = dbResult.rows[0].user_id;
                 res.redirect('/home')
-            }else{
-                res.render('pages/signin-sigup', {singinMsg: 'WrongPass'});
+            } else {
+                res.render('pages/signin-sigup', { singinMsg: 'WrongPass' });
             }
-        }else{
-            res.render('pages/signin-sigup', {singinMsg: 'notExist'});
+        } else {
+            res.render('pages/signin-sigup', { singinMsg: 'notExist' });
         }
-        
+
     })
 }
 /* get data from sign up form */
-function signupFun(req, res){
+function signupFun(req, res) {
     let sql = `select * from users where user_email = '${req.body.Email}';`;
-    client.query(sql).then(result =>{
-        if(result.rows.length > 0){
-            res.render('pages/signin-sigup',{singinMsg: 'defulte'})
-        }else{
-            let {userName,email,password,gender} = req.body;
+    client.query(sql).then(result => {
+        if (result.rows.length > 0) {
+            res.render('pages/signin-sigup', { singinMsg: 'defulte' })
+        } else {
+            let { userName, email, password, gender } = req.body;
             let sqlQuery = `insert into users(user_name,user_email,user_pass,user_gender) values($1,$2,$3,$4);`;
-            let safeValues = [req.body.UserName,req.body.Email,req.body.Password,req.body.gender];
-            client.query(sqlQuery,safeValues).then(()=>{
+            let safeValues = [req.body.UserName, req.body.Email, req.body.Password, req.body.gender];
+            client.query(sqlQuery, safeValues).then(() => {
                 res.redirect('/')
             });
         }
     })
 }
 
-function getSearchResult(req,res){
-    console.log(req.body);
+function getSearchResult(req, res) {
+
+    // console.log(req.body);
     var searchData = req.body.search;
-     var searchStr= searchData.split(' ').join(' OR ');
-     
+    var searchStr = searchData.split(' ').join(' OR ');
+    var userID;
+    let interstId;
+    let intrestarr = [];
+    let finalArray = [];
     //  console.log(searchData.split(' '));
-     let sql=`select * from interests;`;
-     let intrestarr=[];
-     let userID =getUserIdByEmail(req.body.email);
-     client.query(sql).then(dbintrest=>{
-        intrestarr = dbintrest;
-     })
-     searchData.split(' ').forEach(element => {
-         if(intrestarr.includes(element)){
-             let interstId = getinterestIdByEmail(element)
-             if(checkIfexists(userID,interstId)){
+    let sql = `select * from interests;`;
+    client.query(sql)
+        .then(dbintrest => {
+            intrestarr = dbintrest.rows.map(item => {
+                return item['interest_desc'];
+            });
+            console.log('--------------------------------first');
+            console.log('dfghjksd', intrestarr);
+            userID = getUserIdByEmail(user_email)
+                .then((user_id) => {
+                    console.log('user id is here',user_id);
+                    // console.log(searchData.split(' '));
+                    searchData.split(' ').forEach(element => {
+                        if (intrestarr.includes(element)) {
+                            console.log(element);
+                            interstId = getinterestIdByEmail(element)
+                                .then((intrestId) => {
+                                    checkIfexists(user_id, intrestId).then((result) => {
+                                        if (!result) {
+                                            console.log('it is in database');
+                                            let sql = `insert into users_interests (user_id,interest_id) VALUES($1,$2);`;
+                                            let safeValues = [user_id, intrestId];
+                                            return client.query(sql, safeValues)
 
-                 let sql=`insert into users_interests (user_id,interest_id) VALUES($1,$2);`;
-                 let safeValues = [userID,interstId];
-                 client.query(sql,safeValues).then(()=>{
-                    let myURL = `https://newsapi.org/v2/everything?q=(${searchStr})&apiKey=${key}`;
-                    agent.get(myURL).then(apiData=>{
-                       let resultarr = JSON.parse(apiData.text).articles.map(item=>{
-                            return new Article(item);
-                        })
-                        res.render('pages/articls', {articlsKey: resultarr});
+                                        }
+                                    })
+                                })
+                        }
+                    });
+
+                    
+                })
+        })
+        .then(() => {
+            let myURL = `https://newsapi.org/v2/everything?q=(${searchStr})&apiKey=${key}`;
+             agent.get(myURL)
+                .then(apiData => {
+                    let resultarr = JSON.parse(apiData.text).articles.map(item => {
+                        return new Article(item);
                     })
-                 })
-             }
-         }
-     });
-   
+                    console.log('--------------------------------secound');
+                 finalArray = resultarr;
+                 console.log('--------------------------------therids');
+                 res.render('pages/articls', { articlsKey: finalArray });
+                })
+        })
+
+
+
 }
 
-function getUserIdByEmail(str){
-let sql =`select user_id from users where user_email='${str}';`
-client.query(sql).then(userId=>{
-    if(userId.rows.length > 1){
-        return userId.rows[0].user_id;
-    }
-})
+function getUserIdByEmail(str) {
+    let sql = `select user_id from users where user_email='${str}';`
+    return client.query(sql).then(userId => {
+        console.log('momomomomomo', userId.rows[0].user_id);
+        if (userId.rows.length > 0) {
+            return userId.rows[0].user_id;
+        }
+    })
 }
-function getinterestIdByEmail(str){
-let sql =`select interest_id from interests where interest_desc='${str}';`
-client.query(sql).then(userId=>{
-    if(userId.rows.length > 1){
-        return userId.rows[0].interest_id;
-    }
-})
+function getinterestIdByEmail(str) {
+    let sql = `select interest_id from interests where interest_desc='${str}';`
+    return client.query(sql).then(interestid => {
+        if (interestid.rows.length > 0) {
+            console.log('eeeeeeeee',interestid.rows[0].interest_id);
+            return interestid.rows[0].interest_id;
+        }
+    })
 }
-function checkIfexists(userid,intrestid){
-let sql =`select * from users_interests where interest_id='${intrestid}' and user_id='${userid}' ;`
-client.query(sql).then(userId=>{
-    if(userId.rows.length > 1){
-        return true;
-    }else{
-        return false;
-    }
-})
+function checkIfexists(userid, intrestid) {
+    console.log('fffaaa', userid);
+    console.log('rrrrrrrrrrrrrrrrrrrrrr', intrestid);
+    let sql = `select * from users_interests where interest_id=${intrestid} and user_id=${userid};`
+    return client.query(sql).then(userId => {
+        if (userId.rows.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    })
 }
 
 
