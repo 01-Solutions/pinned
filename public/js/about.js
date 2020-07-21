ea3f1c696dffb1e24e073775933f2f0d8c66bcfa
laith <mr.layth_2011@hotmail.com>
2020-07-21 22:26:59 +0300

    editAbout

---
+++ public/js/about.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+////// about //////
+
+$('#parLaith').on('click', () => {
+    console.log('click');
+    $('#laithPar').toggle();
+})
+
+$('#parShella').on('click', () => {
+    console.log('click');
+    $('#shellaPar').toggle();
+})
+$('#parSwedani').on('click', () => {
+    console.log('click');
+    $('#swidaniPar').toggle();
+})
+$('#parIbrahim').on('click', () => {
+    console.log('click');
+    $('#ibrahimPar').toggle();
+})
