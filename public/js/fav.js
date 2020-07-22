efc7c4cb0a9993f407e948436d60918052f0137c
ahmad-swedani <swedani390@gmail.com>
2020-07-22 21:14:38 +0300

    clean code

--- public/js/fav.js
+++ public/js/fav.js
@@ -18,35 +18,8 @@ $('.love').click(function(event) {
     articlDesc = $(this).parents('form').find('#articlDesc').text();
 
     sendDataToBackEnd();
-    // alert(event.target.parentElement.parentElement.parentElement.parentElement.id)
 });
-// sendDataToBackEnd();
-// const ifuser = `<li id="mylist">
-// <a href='/favList' data-item='My List'>My List</a>
-// </li>
 
-// <form id='logOut' action="/signupdata" method="GET">
-
-// <button class="login-btn-alt ">Log-out<i id="logicon" class="fas fa-sign-in-alt"></i>
-// </button>
-
-// </form>`
-
-// /* to check if there data in locale storage that mean the user was signin */
-// function ifuserOrnot() {
-//     if (localStorage.getItem('key')) {
-//         var dataInlocal = JSON.parse(localStorage.getItem('key'))
-//         console.log('done :) ', dataInlocal);
-//         $('#signupdata').remove();
-//         $('.menuItems').append(ifuser);
-//     }
-// }
-// ifuserOrnot();
-
-// $('#logOut').on('click', () => {
-
-//     localStorage.clear();
-// })
 function getlocal() {
 
     if (localStorage.getItem('key')) {
@@ -60,27 +33,9 @@ function getlocal() {
 function sendDataToBackEnd() {
     let localData = getlocal();
     if (localData) {
-        // alert('im in');
-        // console.log('im in');
-        // let artIMG = document.getElementById('artIMG').src;
-        // let cardTag = document.getElementById('cardTag').textContent;
-        // let cardTime = document.getElementById('cardTime').textContent;
-        // let cardUrl = document.getElementById('cardUrl').href;
-        // let cardTitle = document.getElementById('cardTitle').textContent;
-        // let cardAuthor = document.getElementById('cardAuthor').textContent;
-        // let articlDesc = document.getElementById('articlDesc').textContent;
-
-        // console.log(artIMG);
-        // console.log(cardTag);
-        // console.log(cardTime);
-        // console.log(cardUrl);
-        // console.log(cardTitle);
-        // console.log(cardAuthor);
+     
         $.post('/saveFavorate', { articlIMG: artIMG, articlSource: cardTag, articlDes: articlDesc, articlURL: cardUrl, articlTitle: cardTitle,articlDate: cardTime, articlAuthor: cardAuthor });
     } else {
-        // console.log('im out');
-        // alert('im out');
-        // window.redirect('/signupdata');
         window.location.replace('/signupdata');
 
     }
